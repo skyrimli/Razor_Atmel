777c96a2e0514735321755591509d54fbc320369
skyrimli <871937603@qq.com>
2018-03-21 18:21:44 +0800

    1

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -64,7 +64,7 @@ static fnCode_type UserApp1_StateMachine;            /* The state machine functi
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
-
+extern u8 G_au8DebugScanfBuffer[];
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Public functions                                                                                                   */
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -136,7 +136,157 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-
+  static u8 u8Message1[] = "Enter 1 or press button1 to get into state 1\n";
+  static u8 u8Message2[] = "Enter 2 or press button2 to get into state 2\n";
+  static u8 u8Message3[] = "*************************************************\n";
+  static u8 u8LcdMessage1[] = "STATE 1";
+  static u8 u8LcdMessage2[] = "STATE 2";
+  static bool bPrint = TRUE;
+  static u8 u8state = 0;
+  static u8 u8IsAudioOn = FALSE;
+  static u16 u16Counter = 0;
+  static u8 u8aurry[]={0};
+  
+  if(u8state==0)
+  {
+    if(bPrint)
+    {
+      DebugPrintf(u8Message1);
+      DebugPrintf(u8Message2);
+      DebugPrintf(u8Message3);
+      bPrint = FALSE;
+    }
+    if(u8IsAudioOn)
+    {
+      if(u16Counter==0)
+      {
+        PWMAudioOn(BUZZER1);
+      }
+      u16Counter++;
+      if(u16Counter==100)
+      {
+        PWMAudioOff(BUZZER1);
+      }
+      if(u16Counter==1000)
+      {
+        u16Counter=0;
+      }
+      
+    }
+    if(G_au8DebugScanfBuffer[]=='1')
+    {
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON1);
+      LCDClearChars(0,10);
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      LedOn(LCD_RED);
+      LedOn(LCD_GREEN);
+      LedOn(LCD_BLUE);
+      PWMAudioOff(BUZZER1);
+      u8state=1;
+    }
+    if(WasButtonPressed(BUTTON1))
+    {
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON1);
+      LCDClearChars(0,10);
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      LedOn(LCD_RED);
+      LedOn(LCD_GREEN);
+      LedOn(LCD_BLUE);
+      PWMAudioOff(BUZZER1);
+      u8state=1;
+    }
+    if(G_au8DebugScanfBuffer[]=='2')
+    {
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON2);
+      LCDClearChars(0,10);
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      LedOn(LCD_RED);
+      LedOn(LCD_GREEN);
+      LedOn(LCD_BLUE);
+      PWMAudioOff(BUZZER1);
+      u8state=2;
+    }
+    if(WasButtonPressed(BUTTON2))
+    {
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON2);
+      LCDClearChars(0,10);
+      LedOff(WHITE);
+      LedOff(PURPLE);
+      LedOff(BLUE);
+      LedOff(CYAN);
+      LedOff(GREEN);
+      LedOff(YELLOW);
+      LedOff(ORANGE);
+      LedOff(RED);
+      LedOn(LCD_RED);
+      LedOn(LCD_GREEN);
+      LedOn(LCD_BLUE);
+      PWMAudioOff(BUZZER1);
+      u8state=2;
+    }
+    if((G_au8DebugScanfBuffer[]!='1'&&G_au8DebugScanfBuffer[]!='2'))
+    {
+      bPrint = TRUE;
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON1);
+    }
+    if(WasButtonPressed(BUTTON0)||WasButtonPressed(BUTTON3))
+    {
+      bPrint = TRUE;
+      DebugScanf(u8aurry);
+      ButtonAcknowledge(BUTTON1);
+    }
+  }
+  if(u8state==1)
+  {
+    LedOn(WHITE);
+    LedOn(PURPLE);
+    LedOn(BLUE);
+    LedOn(CYAN);
+    LedOff(LCD_BLUE);
+    LCDMessage(0,u8LcdMessage1);
+    u8state=0;
+    u8IsAudioOn = FALSE;
+  }
+  if(u8state==2)
+  {
+    LedBlink(GREEN,LED_1HZ);
+    LedBlink(YELLOW,LED_2HZ);
+    LedBlink(ORANGE,LED_4HZ);
+    LedBlink(RED,LED_8HZ);
+    LedOff(LCD_GREEN);
+    LCDMessage(0,u8LcdMessage2);
+    PWMAudioSetFrequency(BUZZER1,200);
+    u16Counter=0;
+    u8IsAudioOn = TRUE;
+    u8state = 0;
+  }
+  
 } /* end UserApp1SM_Idle() */
     
 
